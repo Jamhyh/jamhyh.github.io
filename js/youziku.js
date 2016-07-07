ad6c4d0bc4a6c80ea2a3492b67f32fe3ccd1ba79
Jamhyh <jamhyh@hotmail.com>
2016-07-08 02:17:34 +0800

    youziku

--- js/youziku.js
+++ js/youziku.js
@@ -1,6 +1,6 @@
 // JavaScript Document
  WebFont.load({
           custom: {
-          urls: ['http://api.youziku.com/webfont/CSS/5770896ef629d80e2c25c587']
+          urls: ['http://api.youziku.com/webfont/CSS/577e9bf8f629d810a83e8476']
      }
  });
