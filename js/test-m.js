0cebce917c6431e64dcc25c1edea96ee499b7ad9
Jamhyh <Jam@yunhaotekiMacBook-Pro.local>
2016-09-13 13:34:42 +0800

    map test

---
+++ js/test-m.js
@@ -0,0 +1,33 @@
+document.getElementById("errmessage").innerHTML = "???";
+var map = new AMap.Map('map-container', {
+    resizeEnable: true,
+    zoom:11,
+    center: [116.397428, 39.90923]
+
+});
+/*
+var options = {
+	enableHighAccuracy: false,
+	timeout: 200000,
+	maximumAge: 30000
+};
+if(navigator.geolocation) {
+	navigator.geolocation.getCurrentPosition(displayPosition, handleError, options);
+}
+else {
+	document.getElementById("errmessage").innerHTML = "shit";
+}
+function displayPosition(pos) {
+  var map = new AMap.Map('map-container', {
+      resizeEnable: true,
+      zoom:11,
+      center: [pos.coords.longitude, pos.coords.latitude]
+
+  });
+}
+document.getElementById("timestamp").innerHTML = pos.timestamp;
+function handleError(err) {
+	document.getElementById("errcode").innerHTML = err.code;
+	document.getElementById("errmessage").innerHTML = err.message;
+}
+*/
